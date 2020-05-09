3612994bdc55caa4ad1a11ac5dbd2e158c5e231f
Jesen Tanadi <mail@jesentanadi.com>
2020-05-09 15:19:52 -0400

    turn off frame

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const path = require("path");
 const { app, BrowserWindow } = electron;
 
 app.on("ready", () => {
-  const windowOption = { width: 800, height: 550 };
+  const windowOption = { width: 800, height: 550, frame: false };
+
   if (process.env.NODE_ENV !== "PRODUCTION" && process.platform !== "darwin") {
     windowOption.height = 575;
+    windowOption.frame = true;
   }
 
   const mainWindow = new BrowserWindow(windowOption);
